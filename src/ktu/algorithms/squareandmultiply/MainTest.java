663c3593d297e0b5eac03a9dfe1bc2e14b0feb94
mcanv <mcanvarer@gmail.com>
2017-04-01 08:03:46 +0300

    Fermatın Testi Yazıldı

--- src/ktu/algorithms/squareandmultiply/MainTest.java
+++ src/ktu/algorithms/squareandmultiply/MainTest.java
@@ -4,6 +4,7 @@ public class MainTest {
 
 	
 	static Square_and_Multiply algorithm = new Square_and_Multiply();
+	static FermatPrimality fermat = new FermatPrimality();
 	
 	public static void main(String[] args) {
 		
@@ -12,7 +13,13 @@ public class MainTest {
 		System.out.println("Sonuc 1 :"+result);
 		int result2 = algorithm.SquareAndMultiply(21, 24, 8);
 		System.out.println("Sonuc 2 : "+result);
-
+		
+		System.out.println();
+		System.out.println("Fermat Asallik testi");
+		int a=10,n=101;
+		if (fermat.FertmatAsallikTesti(a,n)) {
+			System.out.println(n +" Sayisi Asaldir");
+		}else System.out.println(n + " Sayisi Asal degildir.");
 
 	}
 }
