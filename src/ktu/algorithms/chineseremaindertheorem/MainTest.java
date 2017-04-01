ce6802a60f19a9f15a44142ab71748b903a456bd
mcanv <mcanvarer@gmail.com>
2017-04-01 09:43:27 +0300

    maini yazıldı

---
+++ src/ktu/algorithms/chineseremaindertheorem/MainTest.java
@@ -0,0 +1,16 @@
+package ktu.algorithms.chineseremaindertheorem;
+
+public class MainTest {
+
+	static ChineseRemainder chinese = new ChineseRemainder();
+	
+	public static void main(String[] args) {
+		
+		
+		
+		int k=3;
+		int [] a = {2,3,2};
+		int [] m = {3,5,7};
+		chinese.ChineseAlgorithm(k, a, m);
+	}
+}
